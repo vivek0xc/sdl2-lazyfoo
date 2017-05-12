a219e72a9b5aecbea55d5e8b2a8b16012e6bdc37
vivek <vivek0xc@gmail.com>
2017-05-12 18:10:30 +0530

    lesson4

--- 02_getting_an_image_on_the_screen.cpp
+++ 02_getting_an_image_on_the_screen.cpp
@@ -1,16 +1,42 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+enum KeyPressSurfaces
+{
+	KEY_PRESS_SURFACE_DEFAULT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_DOWN,
+	KEY_PRESS_SURFACE_LEFT,
+	KEY_PRESS_SURFACE_RIGHT,
+	KEY_PRESS_SURFACE_TOTAL
+};
 
 bool init();
 bool loadMedia();
+SDL_Surface* loadSurface(std::string path);
+
 void close();
 
 SDL_Window *gWindow = NULL;
 SDL_Surface *gScreenSurface = NULL;
-SDL_Surface *gHelloWorld = NULL;
+SDL_Surface *gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+SDL_Surface *gCurrentSurface = NULL;
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+SDL_Surface *loadSurface(std::string path)
+{
+	SDL_Surface *loadedSurface = SDL_LoadBMP(path.c_str());
+	if(loadedSurface == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		exit(-1);
+	}
+	return loadedSurface;
+}
 
 bool init()
 {
@@ -32,20 +58,19 @@ bool init()
 
 bool loadMedia()
 {
-	gHelloWorld = SDL_LoadBMP("assets/hello_world.bmp");
-	if(gHelloWorld == NULL)
-	{
-		printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
-		return false;
-	}
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("assets/hello_world.bmp");
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("assets/VG0017.BMP");
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("assets/led_strips_MonaLisa.bmp");
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("assets/lena.bmp");
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("assets/testnoise.bmp");
 
 	return true;
 }
 
 void close()
 {
-	SDL_FreeSurface(gHelloWorld);
-	gHelloWorld = NULL;
+	//	SDL_FreeSurface(gHelloWorld);
+	//	gHelloWorld = NULL;
 
 	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
@@ -58,11 +83,12 @@ int main(int argc, char *argv[])
 	bool quit = false;
 	SDL_Event e;
 
+
 	if(init())
 	{
 		if(loadMedia())
 		{
-			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
 
 			while(!quit)
 			{
@@ -72,8 +98,30 @@ int main(int argc, char *argv[])
 					{
 						quit = true;
 					}
-				}
+					else if(e.type == SDL_KEYDOWN)
+					{
+						switch(e.key.keysym.sym)
+						{
+							case SDLK_UP:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_UP];
+								break;
+							case SDLK_DOWN:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+								break;
+							case SDLK_LEFT:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+								break;
+							case SDLK_RIGHT:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+								break;
+							default:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+								break;
+						}
+					}
 
+				}
+				SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
 				SDL_UpdateWindowSurface(gWindow);
 			}
 		}
